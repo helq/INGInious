305f8ec96b809c862ca33d4cb56901221112f847
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2018-09-24 06:34:10 -0500

    Temporarily remove tools

--- inginious/frontend/plugins/multilang/problems/code_multiple_languages_problem.py
+++ inginious/frontend/plugins/multilang/problems/code_multiple_languages_problem.py
@@ -55,4 +55,4 @@ class DisplayableCodeMultipleLanguagesProblem(CodeMultipleLanguagesProblem, Disp
         standard_code_problem_render = super(DisplayableCodeMultipleLanguagesProblem, self).show_input(template_helper, language, seed)
         tools_render = str(renderer.tools(self.get_id(), "plain", custom_input_id, self.get_type(), get_python_tutor_url(), get_linter_url()))
 
-        return multiple_language_render + standard_code_problem_render + tools_render
+        return multiple_language_render + standard_code_problem_render# + tools_render
