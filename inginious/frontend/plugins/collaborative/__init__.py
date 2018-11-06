d6f87c6fa365c56b55a2c364b7eeda2717375135
System Administrator <cgiohidalgo@gmail.com>
2018-11-06 01:38:20 -0500

    modificaction plugin

--- inginious/frontend/plugins/collaborative/__init__.py
+++ inginious/frontend/plugins/collaborative/__init__.py
@@ -1,34 +1,28 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
-# more information about the licensing of this file.
+import os
 
-""" A demo plugin that adds a page """
-from . import pages
+from inginious.frontend.plugins.utils import create_static_resource_page
+from inginious.frontend.plugins.multilang.problems.code_multiple_languages_problem import DisplayableCodeMultipleLanguagesProblem
+from inginious.frontend.plugins.multilang.problems.code_multiple_file_languages_problem import DisplayableCodeFileMultipleLanguagesProblem
+from inginious.frontend.plugins.multilang.problems.constants import set_linter_url, set_python_tutor_url
 
+_static_folder_path = os.path.join(os.path.dirname(__file__), "static")
 
-def init(plugin_manager, _, _2, _3):
-    """ Init the plugin """
+def init(plugin_manager, course_factory, client, plugin_config):
+    plugin_manager.add_page(r'/multilang/static/(.*)', create_static_resource_page(_static_folder_path))
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/multilang.js")
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/pythonTutor.js")
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/grader.js")
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/codemirror_linter.js")
+    plugin_manager.add_hook("javascript_header", lambda: "/multilang/static/lint.js")
+    plugin_manager.add_hook("css", lambda: "/multilang/static/multilang.css")
+    plugin_manager.add_hook("css", lambda: "/multilang/static/lint.css")
+    course_factory.get_task_factory().add_problem_type(DisplayableCodeMultipleLanguagesProblem)
+    course_factory.get_task_factory().add_problem_type(DisplayableCodeFileMultipleLanguagesProblem)
 
-    plugin_manager.add_page(r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring', pages.CourseTaskListPage)
-    plugin_manager.add_page(r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring/task/([a-z0-9A-Z\-_]+)', pages.TaskListSubmissionPage)
-    plugin_manager.add_page(r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring/task/([a-z0-9A-Z\-_]+)/submission/([a-z0-9A-Z\-_]+)',
-                            pages.SubmissionRubricPage)
+    python_tutor_url = plugin_config.get("python_tutor_url", "")
+    if python_tutor_url != "":
+        set_python_tutor_url(python_tutor_url)
 
-
-
-    plugin_manager.add_page(r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring_temp', pages.CourseTaskListPageTemp)
-    plugin_manager.add_page(r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring_temp/task/([a-z0-9A-Z\-_]+)',
-                            pages.TaskListSubmissionPageTemp)
-    plugin_manager.add_page(
-        r'/admin/([a-z0-9A-Z\-_]+)/rubric_scoring_temp/task/([a-z0-9A-Z\-_]+)/submission/([a-z0-9A-Z\-_]+)',
-        pages.SubmissionRubricPageTemp)
-
-
-
-    plugin_manager.add_hook('course_admin_menu', pages.rubric_course_admin_menu_hook)
-    plugin_manager.add_hook('course_admin_menu', pages.rubric_course_admin_menu_hook_temp)
-
-
-
-
+    linter_url = plugin_config.get("linter_url", "")
+    if linter_url != "":
+        set_linter_url(linter_url)
