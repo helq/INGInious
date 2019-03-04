00a60bd5cdc263c6e0b078b70b84b633ddba613d
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-03-03 23:29:10 -0500

    Update usage of read_file function

--- inginious/frontend/plugins/UNCode/__init__.py
+++ inginious/frontend/plugins/UNCode/__init__.py
@@ -1,17 +1,11 @@
 import os
-from inginious.frontend.plugins.utils import create_static_resource_page
+from inginious.frontend.plugins.utils import create_static_resource_page, read_file
 
 _static_folder_path = os.path.join(os.path.dirname(__file__), "static")
 
 _CONTEXT_TASK_TEMPLATE_FILE = "context_task_template.rst"
 _TASK_CONTEXT_HELP_MODAL_HTML_FILE = "task_context_help_modal.html"
 _TASK_FILES_UPLOAD_MULTIPLE_MODAL = "task_files_upload_multiple_modal.html"
-
-
-def read_file(file_name):
-    with open(_static_folder_path + "/" + file_name, "r") as file:
-        content_file = file.read()
-    return content_file
 
 
 def init(plugin_manager, course_factory, client, config):
@@ -21,6 +15,9 @@ def init(plugin_manager, course_factory, client, config):
     plugin_manager.add_hook("javascript_header", lambda: "/UNCode/static/js/task_files_upload_multiple.js")
     plugin_manager.add_hook("css", lambda: "/UNCode/static/css/uncode.css")
     plugin_manager.add_hook("additional_body_html", lambda: "<p class='hidden' id='default_task_context'>" +
-                                                            read_file(_CONTEXT_TASK_TEMPLATE_FILE) + "</p>")
-    plugin_manager.add_hook("additional_body_html", lambda: read_file(_TASK_CONTEXT_HELP_MODAL_HTML_FILE))
-    plugin_manager.add_hook("additional_body_html", lambda: read_file(_TASK_FILES_UPLOAD_MULTIPLE_MODAL))
+                                                            read_file(_static_folder_path,
+                                                                      _CONTEXT_TASK_TEMPLATE_FILE) + "</p>")
+    plugin_manager.add_hook("additional_body_html",
+                            lambda: read_file(_static_folder_path, _TASK_CONTEXT_HELP_MODAL_HTML_FILE))
+    plugin_manager.add_hook("additional_body_html",
+                            lambda: read_file(_static_folder_path, _TASK_FILES_UPLOAD_MULTIPLE_MODAL))
