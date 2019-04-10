b0f393f3e7366912dd09294a7491a5f27816e08c
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-04-10 17:47:45 -0500

    Add option 'show_tools' on multilang plugin show/hide the tools like linter or python tutor (Configurable in configuration.yaml file).

--- inginious/frontend/plugins/multilang/problems/code_multiple_file_languages_problem.py
+++ inginious/frontend/plugins/multilang/problems/code_multiple_file_languages_problem.py
@@ -3,6 +3,7 @@ import os
 from inginious.common.tasks_problems import FileProblem
 from inginious.frontend.task_problems import DisplayableFileProblem
 from .languages import available_languages
+from .constants import get_show_tools
 
 path_to_plugin = os.path.abspath(os.path.dirname(__file__))
 
@@ -46,6 +47,10 @@ class DisplayableCodeFileMultipleLanguagesProblem(CodeFileMultipleLanguagesProbl
 
         multiple_language_render = str(renderer.multilang(self.get_id(), dropdown_id, allowed_languages, self.get_id(), self.get_type()))
         standard_code_problem_render = super(DisplayableCodeFileMultipleLanguagesProblem, self).show_input(template_helper, language, seed)
-        tools_render = str(renderer.tools(self.get_id(), "plain", custom_input_id, self.get_type(), "python_tutor_url", "linter_url"))
+        tools_render = ""
+        if get_show_tools():
+            tools_render = str(
+                renderer.tools(self.get_id(), "plain", custom_input_id, self.get_type(), "python_tutor_url",
+                               "linter_url"))
 
         return multiple_language_render + standard_code_problem_render + tools_render
--- inginious/frontend/plugins/multilang/problems/constants.py
+++ inginious/frontend/plugins/multilang/problems/constants.py
@@ -1,16 +1,30 @@
 _python_tutor_url = "http://localhost:8003/"
 _linter_url = "http://localhost:4567/"
+_show_tools = True
+
 
 def set_python_tutor_url(new_python_tutor_url):
     global _python_tutor_url
     _python_tutor_url = new_python_tutor_url
 
+
 def set_linter_url(new_linter_url):
     global _linter_url
     _linter_url = new_linter_url
 
+
+def set_show_tools(new_show_tools_value):
+    global _show_tools
+    _show_tools = new_show_tools_value
+
+
 def get_python_tutor_url():
     return _python_tutor_url
 
+
 def get_linter_url():
     return _linter_url
+
+
+def get_show_tools():
+    return _show_tools
