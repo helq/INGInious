68f7fcc1331ae836d48f203d4306668dd74b74e3
Juan David Valencia <judvalenciaan@unal.edu.co>
2019-05-01 18:53:12 -0500

    Adding the message, and bug fix

--- inginious/frontend/plugins/grader_generator/static/js/grader_generator.js
+++ inginious/frontend/plugins/grader_generator/static/js/grader_generator.js
@@ -1,5 +1,7 @@
 let studio_grader_test_case_sequence = 0;
 let grader_test_cases_count = 0;
+let test_cases_input = [];
+let ids_test_cases_input = [];
 
 function studio_add_test_case_from_form()
 {
@@ -39,6 +41,8 @@ function studio_add_test_case(test_case)
 
     studio_grader_test_case_sequence++;
     grader_test_cases_count++;
+    test_cases_input.push(inputFile);
+    ids_test_cases_input.push(test_id)
 
     let first_row = (grader_test_cases_count == 1);
 
@@ -61,6 +65,9 @@ function studio_remove_test_case(id) {
     if(grader_test_cases_count == 0){
       $('#grader_test_cases_header').hide();
     }
+    let ind_of_test_case = ids_test_cases_input.findIndex(el => el === id);
+    test_cases_input.splice(ind_of_test_case, 1);
+    ids_test_cases_input.splice(ind_of_test_case, 1);
 }
 
 function studio_update_grader_problems() {
@@ -178,29 +185,32 @@ function read_files_and_match(){
     task_id: taskId
 }, function(files) {
     // Pass the file info to JSON for comparison
-    var json_files = [];
-    for(var ind = 0; ind < files.length; ind++)
+    let json_files = [];
+    for(let ind = 0; ind < files.length; ind++)
       json_files.push(JSON.stringify(files[ind]));
-
-    // Check the current test cases for no repetitions
-    var test_cases = []
-    for(ind = 0; ind < grader_test_cases_count; ind++)
-      test_cases.push($("#grader_test_cases_"+ ind + "_input_file").val());
-
+    
     $.each(files, function(index, file) {
-      if (file.is_directory || test_cases.includes(file.complete_name)) {
+      if (file.is_directory ) {
         return;
       }
-      var entry = {};
-      var parts = file.name.split('.');
-      var complete_parts = file.complete_name.split('.')
+      
+      let entry = {};
+      let parts = file.name.split('.');
+      let complete_parts = file.complete_name.split('.')
+      let name_without_extension = parts.splice(0, parts.length - 1).join(".");
+      let complete_name_output = complete_parts.splice(0, complete_parts.length - 1).join(".") + '.out';
+      
+      
+      if(test_cases_input.includes(file.name)){
+        return;
+      }
       if (parts[parts.length - 1] === 'in'){
-        name_without_extension = parts.splice(0, parts.length - 1).join(".");
-        var file_obj = {
+        
+        let file_obj = {
           "level" : file.level,
           "is_directory" : false,
           "name" : name_without_extension + '.out',
-          "complete_name" : complete_parts.splice(0, complete_parts.length - 1).join(".") + '.out'
+          "complete_name" : complete_name_output
         }
         if (json_files.includes(JSON.stringify(file_obj))){
           entry = {
